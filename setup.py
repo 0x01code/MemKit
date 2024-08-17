aca78b84f069165fbe648f52c2c1eaafe3408327
0x01code <admin@0x01code.com>
2024-08-17 21:07:12 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='memkit',
-    version='0.0.4',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'memprocfs'
